f18c95936fb06af1e773c6b3cb6fb846d3aacc10
suke123 <14ec066@ms.dendai.ac.jp>
2017-05-03 12:30:07 +0900

    Edit

--- Shooting_Sample/app/src/main/java/com/example/shooting_sample/TekiLogic.java
+++ Shooting_Sample/app/src/main/java/com/example/shooting_sample/TekiLogic.java
@@ -9,7 +9,7 @@ import com.example.shooting_sample.mono.Teki;
  */
 
 public class TekiLogic {
-    private static double period = 1000;
+    private static double period = 10;
     private final Context context;
     private final HanteiList<Mono> list;
     private double tic;
@@ -20,7 +20,7 @@ public class TekiLogic {
         list.add(createTeki());
     }
     private Mono createTeki() {
-        return new Teki(context, 200, 200);
+        return new Teki(context, 200, 30);
     }
     public void step(double tstep, int width, int height) {
         tic += tstep;
