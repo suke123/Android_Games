7f128d07ce1dac21d48726570e644a9941eb6c99
suke123 <14ec066@ms.dendai.ac.jp>
2017-05-08 16:38:11 +0900

    Edit files and add new project

---
+++ SlideshowApp/app/src/main/java/jp/ac/dendai/c/jtp/slideshowapp/MainActivity.java
@@ -0,0 +1,21 @@
+package jp.ac.dendai.c.jtp.slideshowapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import android.widget.ImageView;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
