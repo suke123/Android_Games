0bcc95e64dd1531b097135753c85f8b0a1f8b8e7
suke123 <14ec066@ms.dendai.ac.jp>
2017-05-15 13:41:15 +0900

    Edit

--- SlideshowApp/app/src/main/java/jp/ac/dendai/c/jtp/slideshowapp/MainActivity.java
+++ SlideshowApp/app/src/main/java/jp/ac/dendai/c/jtp/slideshowapp/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                                 isFlip = true;
                                 viewFlipper.setInAnimation(AnimationUtils.loadAnimation(this, R.anim.move_in_left));
                                 viewFlipper.setInAnimation(AnimationUtils.loadAnimation(this, R.anim.move_out_right));
-                                textView.setText(files.length);
+                                textView.setText("{files.length}");
                                 viewFlipper.showNext();
                             } else if (firstTouch - x > 50) {
                                 isFlip = true;
