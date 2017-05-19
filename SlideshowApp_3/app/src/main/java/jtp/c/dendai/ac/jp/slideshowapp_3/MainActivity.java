f1d0eb94ec74a5e340d8b22177c59ad991719a42
suke123 <14ec066@ms.dendai.ac.jp>
2017-05-19 09:46:42 +0900

    Edit

--- SlideshowApp_3/app/src/main/java/jtp/c/dendai/ac/jp/slideshowapp_3/MainActivity.java
+++ SlideshowApp_3/app/src/main/java/jtp/c/dendai/ac/jp/slideshowapp_3/MainActivity.java
@@ -2,6 +2,8 @@ package jtp.c.dendai.ac.jp.slideshowapp_3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setFileId();
+
+        TextView tv = (TextView)findViewById(R.id.text_view);
+
+    }
+
+    private void setFileId() {
+        File dir = 
     }
 }
