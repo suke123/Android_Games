1c1bae2da7fb2cf61ddd2702891a95f034c3db67
suke123 <14ec066@ms.dendai.ac.jp>
2017-05-20 10:33:09 +0900

    edit

--- SlideshowApp_3/app/src/main/java/jtp/c/dendai/ac/jp/slideshowapp_3/MainActivity.java
+++ SlideshowApp_3/app/src/main/java/jtp/c/dendai/ac/jp/slideshowapp_3/MainActivity.java
@@ -29,7 +29,15 @@ public class MainActivity extends AppCompatActivity {
             try {
                 //フィールド名取得
                 String name = field.getName();
+                //フィールドの値を取得する
+                int value = (Integer) field.get(name);
+                //画像読み込み
+                imageView.setImageResource(value);
 
+            }catch (IllegalArgumentException e){
+                e.printStackTrace();
+            }catch (IllegalAccessException e){
+                e.printStackTrace();
             }
         }
     }
