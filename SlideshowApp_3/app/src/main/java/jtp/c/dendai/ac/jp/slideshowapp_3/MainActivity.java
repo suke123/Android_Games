3b92080a287107a51f530d6721525509b47b506d
suke123 <14ec066@ms.dendai.ac.jp>
2017-05-20 10:23:00 +0900

    edit

--- SlideshowApp_3/app/src/main/java/jtp/c/dendai/ac/jp/slideshowapp_3/MainActivity.java
+++ SlideshowApp_3/app/src/main/java/jtp/c/dendai/ac/jp/slideshowapp_3/MainActivity.java
@@ -6,20 +6,39 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.lang.reflect.Field;
+
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Field[] fields;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView imageView;
-        //setFileId();
-
+        setImageView();         //ImageViewを
+        getImageResources();    //画像リソース取得
+        readImageResources();   //画像リソース読み込みyomikomi
         //TextView tv = (TextView)findViewById(R.id.text_view);
 
     }
 
-    /*private void setFileId() {
-        File file= new File();
-    }*/
+    private void readImageResources() {
+        for (Field field : fields) {
+            try {
+                //フィールド名取得
+                String name = field.getName();
+
+            }
+        }
+    }
+
+    private void setImageView() {
+        imageView = (ImageView) findViewById(R.id.slideshow_imageview);
+    }
+
+    private void getImageResources() {
+        fields = R.drawable.class.getFields();
+    }
 }
