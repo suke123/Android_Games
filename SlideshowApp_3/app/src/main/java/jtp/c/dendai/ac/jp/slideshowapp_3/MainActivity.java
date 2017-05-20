3546d967b346b83c5c0f5f06b643323043ae74e3
suke123 <14ec066@ms.dendai.ac.jp>
2017-05-20 14:32:53 +0900

    edit

--- SlideshowApp_3/app/src/main/java/jtp/c/dendai/ac/jp/slideshowapp_3/MainActivity.java
+++ SlideshowApp_3/app/src/main/java/jtp/c/dendai/ac/jp/slideshowapp_3/MainActivity.java
@@ -2,14 +2,20 @@ package jtp.c.dendai.ac.jp.slideshowapp_3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 import java.lang.reflect.Field;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
+    private ViewFlipper viewFlipper;
+    private float firstTouch;
+    private boolean isFlip = false;
     ImageView imageView;
     Field[] fields;
 
@@ -23,6 +29,44 @@ public class MainActivity extends AppCompatActivity {
         //TextView tv = (TextView)findViewById(R.id.text_view);
 
     }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        //isImageTouched();
+        int x = (int) event.getRawX();
+        //imageVisibility();
+        switch (v.getId()) {
+            //case R.id.layout_first:
+            case R.id.slideshow_imageview:
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        firstTouch = event.getRawX();
+                        return true;
+                    case MotionEvent.ACTION_MOVE:
+                        if (!isFlip) {
+                            if (x - firstTouch > 50) {
+                                isFlip = true;
+                                viewFlipper.setInAnimation(AnimationUtils.loadAnimation(this, R.anim.move_in_left));
+                                //viewFlipper.setInAnimation(AnimationUtils.loadAnimation(this, R.anim.move_out_right));
+//                                textView.setText(files.length);
+                                viewFlipper.showNext();
+                            } else if (firstTouch - x > 50) {
+                                isFlip = true;
+                                viewFlipper.setInAnimation(AnimationUtils.loadAnimation(this, R.anim.move_in_right));
+                                //viewFlipper.setInAnimation(AnimationUtils.loadAnimation(this, R.anim.move_out_left));
+                                viewFlipper.showPrevious();
+                            }
+                        }
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        isFlip = false;
+                        break;
+                }
+        }
+
+        return false;
+    }
+
 
     private void readImageResources() {
         for (Field field : fields) {
