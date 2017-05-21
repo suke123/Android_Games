11c23c2c97b02a0a8b8cff52d093879a28af5846
suke123 <14ec066@ms.dendai.ac.jp>
2017-05-21 10:00:59 +0900

    Edit

--- SlideshowApp_3/app/src/main/java/jtp/c/dendai/ac/jp/slideshowapp_3/MainActivity.java
+++ SlideshowApp_3/app/src/main/java/jtp/c/dendai/ac/jp/slideshowapp_3/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     TextView fieldText;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setImageView();         //ImageViewを
