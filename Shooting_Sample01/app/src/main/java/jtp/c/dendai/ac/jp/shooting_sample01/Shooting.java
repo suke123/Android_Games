2cccf037f07a6e71d078e49fde4d0adc77e8efdf
suke123 <14ec066@ms.dendai.ac.jp>
2017-05-27 19:26:21 +0900

    Edit

---
+++ Shooting_Sample01/app/src/main/java/jtp/c/dendai/ac/jp/shooting_sample01/Shooting.java
@@ -0,0 +1,14 @@
+package jtp.c.dendai.ac.jp.shooting_sample01;
+
+import android.app.Activity;
+
+/**
+ * Created by taka on 2017/05/27.
+ */
+
+class Shooting {
+
+    public shooting(Activity a){
+        
+    }
+}
