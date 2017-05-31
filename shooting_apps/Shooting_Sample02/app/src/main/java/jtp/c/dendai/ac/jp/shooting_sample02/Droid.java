d49ff91dd541cea8ea4dd6f1d4c4e28fe8b1abc3
suke123 <14ec066@ms.dendai.ac.jp>
2017-06-01 02:36:28 +0900

    Edit

---
+++ shooting_apps/Shooting_Sample02/app/src/main/java/jtp/c/dendai/ac/jp/shooting_sample02/Droid.java
@@ -0,0 +1,31 @@
+package jtp.c.dendai.ac.jp.shooting_sample02;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+
+/**
+ * Created by taka on 2017/06/01.
+ */
+
+public class Droid {
+    private final Paint paint = new Paint();
+
+    public final Bitmap bitmap;
+    public final Rect rect;
+
+    public Droid(Bitmap bitmap, int width, int height) {
+        this.bitmap = bitmap;
+
+        int left = (width - bitmap.getWidth()) / 2;
+        int top = (height - bitmap.getHeight()) / 2;
+        int right = left + bitmap.getWidth();
+        int bottom = top + bitmap.getHeight();
+        rect = new Rect(left, top, right, bottom);
+    }
+
+    public void draw(Canvas canvas) {
+        canvas.drawBitmap(bitmap, rect.left, rect.top, paint);
+    }
+}
---
+++ shooting_apps/Shooting_Sample02/app/src/main/java/jtp/c/dendai/ac/jp/shooting_sample02/GameView.java
@@ -0,0 +1,24 @@
+package jtp.c.dendai.ac.jp.shooting_sample02;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+/**
+ * Created by taka on 2017/06/01.
+ */
+
+public class GameView extends View {
+    private Droid droid;
+
+    public GameView(Context context){
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+
+        
+    }
+}
