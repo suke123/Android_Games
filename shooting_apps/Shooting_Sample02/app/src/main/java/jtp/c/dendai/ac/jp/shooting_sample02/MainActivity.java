2532ad4a503226bd51cd72310e44b3c48265eab0
suke123 <14ec066@ms.dendai.ac.jp>
2017-05-31 12:52:18 +0900

    Edit

---
+++ shooting_apps/Shooting_Sample02/app/src/main/java/jtp/c/dendai/ac/jp/shooting_sample02/MainActivity.java
@@ -0,0 +1,13 @@
+package jtp.c.dendai.ac.jp.shooting_sample02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
