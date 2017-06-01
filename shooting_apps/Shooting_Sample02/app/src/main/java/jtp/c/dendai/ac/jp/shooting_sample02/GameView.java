83fc3580c31e147cc7b55396f4adbbeacc3d96a4
suke123 <14ec066@ms.dendai.ac.jp>
2017-06-01 09:22:43 +0900

    Edit

--- shooting_apps/Shooting_Sample02/app/src/main/java/jtp/c/dendai/ac/jp/shooting_sample02/GameView.java
+++ shooting_apps/Shooting_Sample02/app/src/main/java/jtp/c/dendai/ac/jp/shooting_sample02/GameView.java
@@ -1,6 +1,8 @@
 package jtp.c.dendai.ac.jp.shooting_sample02;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.view.View;
 
@@ -11,14 +13,22 @@ import android.view.View;
 public class GameView extends View {
     private Droid droid;
 
-    public GameView(Context context){
+    public GameView(Context context) {
         super(context);
     }
 
     @Override
-    protected void onDraw(Canvas canvas){
+    protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        
+        int width = canvas.getWidth();
+        int height = canvas.getHeight();
+
+        if (droid == null) {
+            Bitmap droidBitmap = BitmapFactory.decodeResource(getResources().R.drawable.droid);
+            droid = new Droid(droidBitmap, width, height);
+        }
+
+        droid.draw(canvas);
     }
 }
--- shooting_apps/Shooting_Sample02/app/src/main/java/jtp/c/dendai/ac/jp/shooting_sample02/MainActivity.java
+++ shooting_apps/Shooting_Sample02/app/src/main/java/jtp/c/dendai/ac/jp/shooting_sample02/MainActivity.java
@@ -4,10 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private GameView gameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        gameView = new GameView(this);
+        setContentView(gameView);
     }
 }
