0548ea814a2a99ebe74e618be5749919badf6807
suke123 <14ec066@ms.dendai.ac.jp>
2017-06-01 19:54:09 +0900

    Edit

--- shooting_apps/Shooting_Sample02/app/src/main/java/jtp/c/dendai/ac/jp/shooting_sample02/GameView.java
+++ shooting_apps/Shooting_Sample02/app/src/main/java/jtp/c/dendai/ac/jp/shooting_sample02/GameView.java
@@ -6,12 +6,19 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by taka on 2017/06/01.
  */
 
 public class GameView extends View {
+    private static final int MISSILE_LAUNCH_WEIGHT = 50;
     private Droid droid;
+    private final List<BaseObject> missileList = new ArrayList<>();
+    private final Random rand = new Random(System.currentTimeMillis());
 
     public GameView(Context context) {
         super(context);
@@ -29,6 +36,35 @@ public class GameView extends View {
             droid = new Droid(droidBitmap, width, height);
         }
 
+        if (rand.nextInt(MISSILE_LAUNCH_WEIGHT) == 0) {
+            Missile missile = launchMissile(width, height);
+            missileList.add(missile);
+        }
+        drawObjectList(canvas, missileList, width, height);
+
         droid.draw(canvas);
+        invalidate();
+    }
+
+    private static void drawObjectList(
+            Canvas canvas, List<BaseObject> objectList, int width, int height) {
+        for (int i = 0; i < objectList.size(); i++) {
+            BaseObject object = objectList.get(i);
+            if (object.isAvailable(width, height)) {
+                object.move();
+                object.draw(canvas);
+            } else {
+                objectList.remove(object);
+                i--;
+            }
+        }
+    }
+
+    private Missile launchMissile(int width, int height) {
+        int fromX = rand.nextInt(width);
+        int toX = rand.nextInt(height);
+
+        float alignX = (toX - fromX) / (float) height;
+        return new Missile(fromX, alignX);
     }
 }
