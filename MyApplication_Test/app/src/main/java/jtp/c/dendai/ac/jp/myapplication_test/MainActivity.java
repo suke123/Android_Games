3b2a65f8bb3388aa7536ca79afd05b090b5c3a74
suke123 <14ec066@ms.dendai.ac.jp>
2017-07-18 14:18:45 +0900

    add

---
+++ MyApplication_Test/app/src/main/java/jtp/c/dendai/ac/jp/myapplication_test/MainActivity.java
@@ -0,0 +1,16 @@
+package jtp.c.dendai.ac.jp.myapplication_test;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by taka on 2017/07/09.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
